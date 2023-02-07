447752e8db06a9c21195803c1bb3b5a57daabf5b
Thiago Castro <thiagovieirac@gmail.com>
2023-02-07 19:26:11 -0300

    Função para criar DF, tratar coluna de dada e enviar para DB

--- db.py
+++ db.py
@@ -8,16 +8,32 @@ from sqlalchemy.orm import sessionmaker
 from devices_thread import get_devices
 
 
-params = urllib.parse.quote_plus(
-    r"Driver={ODBC Driver 18 for SQL Server};Server=tcp:server-name.database.windows.net,1433;Database=maas_360;Uid=user;Pwd=password!;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
-)
-conn_str = "mssql+pyodbc:///?odbc_connect={}".format(params)
-engine_azure = create_engine(conn_str, echo=True)
+def create_db_engine_object():
+    try:
+        load_dotenv()
+        DB_SERVER = os.environ.get("DB_SERVER")
+        DATABASE = os.environ.get("DATABASE")
+        DB_USER = os.environ.get("DB_USER")
+        DB_PSWD = os.environ.get("DB_PSWD")
+        params = urllib.parse.quote_plus(
+            rf"Driver={{ODBC Driver 18 for SQL Server}};Server=tcp:{DB_SERVER}.database.windows.net,1433;Database={DATABASE};Uid={DB_USER};Pwd={DB_PSWD};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
+        )
+        conn_str = "mssql+pyodbc:///?odbc_connect={}".format(params)
+        engine = create_engine(conn_str, echo=True)
+        return engine
+    except Exception as e:
+        print(f"Erro ao criar o objeto de engine: {e}")
+        return None
 
 
-print("connection is ok")
+def dataframe_to_db(db_engine=create_db_engine_object()):
+    devices = get_devices()
+    data = pd.DataFrame(devices)
 
-devices = get_devices()
+    data["lastReported"] = pd.to_datetime(data["lastReported"])
+    data["lastReported"] = data["lastReported"].dt.strftime("%d/%m/%Y")
+    data.to_sql("devices", db_engine, if_exists="replace")
 
-data = pd.DataFrame(devices)
-data.to_sql("devices", engine_azure)
+
+if __name__ == "__main__":
+    dataframe_to_db()
